08159ff15d7a9628fb6a8aa03347671150dbcd9c
Laurrencia <elaurrencia27@gmail.com>
2023-09-08 15:40:38 +0400

    Intial Commit : Setting date and time

---
+++ main.js
@@ -0,0 +1,15 @@
+function getCurrentTime(){
+    const days =[ "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
+    const todaysDate = new Date();
+    const dayOfWeek = days[todaysDate.getUTCDay()];
+    const time = todaysDate.toISOString().slice(11, 23);
+
+    const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
+
+    currentDay.textContent = dayOfWeek;
+    currentTime.textContent = time;
+
+    setTimeout(getCurrentTime, 1000);
+}
+getCurrentTime();
