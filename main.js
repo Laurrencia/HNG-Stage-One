df6b76ab5d67c6fb3369ae1da414ea73d15e0deb
Laurrencia <elaurrencia27@gmail.com>
2023-09-08 16:02:15 +0400

    changed tag

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getCurrentTime(){
     const days =[ "Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"];
     const todaysDate = new Date();
     const dayOfWeek = days[todaysDate.getUTCDay()];
-    const time = todaysDate.toISOString().slice(11, 23);
+    const time = todaysDate.toISOString().slice(11, 21);
 
     const currentDay = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentTime = document.querySelector('[data-testid="currentUTCTime"]');
@@ -10,6 +10,6 @@ function getCurrentTime(){
     currentDay.textContent = dayOfWeek;
     currentTime.textContent = time;
 
-    setTimeout(getCurrentTime, 1000);
+    setTimeout(getCurrentTime,  100000);
 }
 getCurrentTime();
